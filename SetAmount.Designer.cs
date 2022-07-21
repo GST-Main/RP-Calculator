99e91306beb6df678ed78a02a5bcce46631d3657
성택 권 <kkst9602@naver.com>
2022-07-21 21:15:11 +0900

    Add A Setting Charging Amount Form

---
+++ SetAmount.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace RP_Calculator
+{
+	partial class form_SetAmount
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox_setKRW0 = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.textBox_setKRW1 = new System.Windows.Forms.TextBox();
+			this.textBox_setKRW2 = new System.Windows.Forms.TextBox();
+			this.textBox_setKRW3 = new System.Windows.Forms.TextBox();
+			this.textBox_setKRW4 = new System.Windows.Forms.TextBox();
+			this.textBox_setKRW5 = new System.Windows.Forms.TextBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.textBox_setRP5 = new System.Windows.Forms.TextBox();
+			this.textBox_setRP4 = new System.Windows.Forms.TextBox();
+			this.textBox_setRP3 = new System.Windows.Forms.TextBox();
+			this.textBox_setRP2 = new System.Windows.Forms.TextBox();
+			this.textBox_setRP1 = new System.Windows.Forms.TextBox();
+			this.textBox_setRP0 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button_CancelSetCharge = new System.Windows.Forms.Button();
+			this.button_ApplySetCharge = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textBox_setKRW0
+			// 
+			this.textBox_setKRW0.Location = new System.Drawing.Point(6, 22);
+			this.textBox_setKRW0.Name = "textBox_setKRW0";
+			this.textBox_setKRW0.Size = new System.Drawing.Size(100, 23);
+			this.textBox_setKRW0.TabIndex = 0;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.textBox_setKRW5);
+			this.groupBox1.Controls.Add(this.textBox_setKRW4);
+			this.groupBox1.Controls.Add(this.textBox_setKRW3);
+			this.groupBox1.Controls.Add(this.textBox_setKRW2);
+			this.groupBox1.Controls.Add(this.textBox_setKRW1);
+			this.groupBox1.Controls.Add(this.textBox_setKRW0);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(115, 201);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "충전 금액 (원)";
+			// 
+			// textBox_setKRW1
+			// 
+			this.textBox_setKRW1.Location = new System.Drawing.Point(6, 51);
+			this.textBox_setKRW1.Name = "textBox_setKRW1";
+			this.textBox_setKRW1.Size = new System.Drawing.Size(100, 23);
+			this.textBox_setKRW1.TabIndex = 0;
+			// 
+			// textBox_setKRW2
+			// 
+			this.textBox_setKRW2.Location = new System.Drawing.Point(6, 80);
+			this.textBox_setKRW2.Name = "textBox_setKRW2";
+			this.textBox_setKRW2.Size = new System.Drawing.Size(100, 23);
+			this.textBox_setKRW2.TabIndex = 0;
+			// 
+			// textBox_setKRW3
+			// 
+			this.textBox_setKRW3.Location = new System.Drawing.Point(6, 109);
+			this.textBox_setKRW3.Name = "textBox_setKRW3";
+			this.textBox_setKRW3.Size = new System.Drawing.Size(100, 23);
+			this.textBox_setKRW3.TabIndex = 0;
+			// 
+			// textBox_setKRW4
+			// 
+			this.textBox_setKRW4.Location = new System.Drawing.Point(6, 138);
+			this.textBox_setKRW4.Name = "textBox_setKRW4";
+			this.textBox_setKRW4.Size = new System.Drawing.Size(100, 23);
+			this.textBox_setKRW4.TabIndex = 0;
+			// 
+			// textBox_setKRW5
+			// 
+			this.textBox_setKRW5.Location = new System.Drawing.Point(6, 167);
+			this.textBox_setKRW5.Name = "textBox_setKRW5";
+			this.textBox_setKRW5.Size = new System.Drawing.Size(100, 23);
+			this.textBox_setKRW5.TabIndex = 0;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.textBox_setRP5);
+			this.groupBox2.Controls.Add(this.textBox_setRP4);
+			this.groupBox2.Controls.Add(this.textBox_setRP3);
+			this.groupBox2.Controls.Add(this.textBox_setRP2);
+			this.groupBox2.Controls.Add(this.textBox_setRP1);
+			this.groupBox2.Controls.Add(this.textBox_setRP0);
+			this.groupBox2.Location = new System.Drawing.Point(157, 12);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(115, 201);
+			this.groupBox2.TabIndex = 2;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "충전 RP";
+			// 
+			// textBox_setRP5
+			// 
+			this.textBox_setRP5.Location = new System.Drawing.Point(6, 167);
+			this.textBox_setRP5.Name = "textBox_setRP5";
+			this.textBox_setRP5.Size = new System.Drawing.Size(100, 23);
+			this.textBox_setRP5.TabIndex = 0;
+			// 
+			// textBox_setRP4
+			// 
+			this.textBox_setRP4.Location = new System.Drawing.Point(6, 138);
+			this.textBox_setRP4.Name = "textBox_setRP4";
+			this.textBox_setRP4.Size = new System.Drawing.Size(100, 23);
+			this.textBox_setRP4.TabIndex = 0;
+			// 
+			// textBox_setRP3
+			// 
+			this.textBox_setRP3.Location = new System.Drawing.Point(6, 109);
+			this.textBox_setRP3.Name = "textBox_setRP3";
+			this.textBox_setRP3.Size = new System.Drawing.Size(100, 23);
+			this.textBox_setRP3.TabIndex = 0;
+			// 
+			// textBox_setRP2
+			// 
+			this.textBox_setRP2.Location = new System.Drawing.Point(6, 80);
+			this.textBox_setRP2.Name = "textBox_setRP2";
+			this.textBox_setRP2.Size = new System.Drawing.Size(100, 23);
+			this.textBox_setRP2.TabIndex = 0;
+			// 
+			// textBox_setRP1
+			// 
+			this.textBox_setRP1.Location = new System.Drawing.Point(6, 51);
+			this.textBox_setRP1.Name = "textBox_setRP1";
+			this.textBox_setRP1.Size = new System.Drawing.Size(100, 23);
+			this.textBox_setRP1.TabIndex = 0;
+			// 
+			// textBox_setRP0
+			// 
+			this.textBox_setRP0.Location = new System.Drawing.Point(6, 22);
+			this.textBox_setRP0.Name = "textBox_setRP0";
+			this.textBox_setRP0.Size = new System.Drawing.Size(100, 23);
+			this.textBox_setRP0.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(133, 100);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(18, 15);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "→";
+			// 
+			// button_CancelSetCharge
+			// 
+			this.button_CancelSetCharge.BackColor = System.Drawing.Color.LightSalmon;
+			this.button_CancelSetCharge.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.button_CancelSetCharge.Location = new System.Drawing.Point(12, 219);
+			this.button_CancelSetCharge.Name = "button_CancelSetCharge";
+			this.button_CancelSetCharge.Size = new System.Drawing.Size(115, 23);
+			this.button_CancelSetCharge.TabIndex = 4;
+			this.button_CancelSetCharge.Text = "취소";
+			this.button_CancelSetCharge.UseVisualStyleBackColor = false;
+			// 
+			// button_ApplySetCharge
+			// 
+			this.button_ApplySetCharge.Location = new System.Drawing.Point(157, 219);
+			this.button_ApplySetCharge.Name = "button_ApplySetCharge";
+			this.button_ApplySetCharge.Size = new System.Drawing.Size(115, 23);
+			this.button_ApplySetCharge.TabIndex = 5;
+			this.button_ApplySetCharge.Text = "설정";
+			this.button_ApplySetCharge.UseVisualStyleBackColor = true;
+			// 
+			// form_SetAmount
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 256);
+			this.Controls.Add(this.button_ApplySetCharge);
+			this.Controls.Add(this.button_CancelSetCharge);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "form_SetAmount";
+			this.ShowIcon = false;
+			this.Text = "충전 금액 설정";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private TextBox textBox_setKRW0;
+		private GroupBox groupBox1;
+		private TextBox textBox_setKRW5;
+		private TextBox textBox_setKRW4;
+		private TextBox textBox_setKRW3;
+		private TextBox textBox_setKRW2;
+		private TextBox textBox_setKRW1;
+		private GroupBox groupBox2;
+		private TextBox textBox_setRP5;
+		private TextBox textBox_setRP4;
+		private TextBox textBox_setRP3;
+		private TextBox textBox_setRP2;
+		private TextBox textBox_setRP1;
+		private TextBox textBox_setRP0;
+		private Label label1;
+		private Button button_CancelSetCharge;
+		private Button button_ApplySetCharge;
+	}
+}
